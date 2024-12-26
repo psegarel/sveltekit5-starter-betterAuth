4c47a9dd805eb0fa13f9e626892ea571f8a43aad
Patrick <contact@patrick-segarel.com>
2024-12-26 22:02:44 +0700

    removes unused components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,2 @@
 <script lang="ts">
-	import AppSidebar from '$lib/components/app-sidebar.svelte';
-	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
-	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import SidebarFooter from '$lib/components/ui/sidebar/sidebar-footer.svelte';
-
-	const title = 'Starter Template';
 </script>
