ffe86728bbb3eb8721ce476ca3e1eac2692cc0a6
Patrick <contact@patrick-segarel.com>
2025-01-01 21:05:35 +0700

    Contact form with Card

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import ContactFormComponent from './ContactFormComponent.svelte';
+	import ContactForm from './ContactForm.svelte';
 
 	let { data }: { data: PageData } = $props();
 </script>
@@ -8,6 +8,6 @@
 <div class="grid h-screen w-screen grid-cols-1 items-center justify-center md:grid-cols-2">
 	<div class="hidden h-screen w-full bg-zinc-900 md:block"></div>
 	<div class="flex flex-col items-center justify-center">
-		<ContactFormComponent data={data.form} />
+		<ContactForm data={data.form} />
 	</div>
 </div>
---
+++ src/routes/contact/ContactForm.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import * as Form from '$lib/components/ui/form/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Textarea } from '$lib/components/ui/textarea/index.js';
+
+	import { contactFormSchema, type ContactFormSchema } from './contact-form-schema';
+	import { type SuperValidated, type Infer, superForm } from 'sveltekit-superforms';
+	import { zodClient } from 'sveltekit-superforms/adapters';
+
+	let { data }: { data: SuperValidated<Infer<ContactFormSchema>> } = $props();
+
+	const form = superForm(data, {
+		validators: zodClient(contactFormSchema)
+	});
+	const { form: formData, enhance } = form;
+</script>
+
+<Card.Root class="mx-auto w-full border-none shadow-none">
+	<Card.Header>
+		<Card.Title class="text-2xl">Contact</Card.Title>
+		<Card.Description>Please use the form below to send us any enquiries</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<form method="POST" class="w-full space-y-4" use:enhance>
+			<Form.Field {form} name="name">
+				<Form.Control>
+					{#snippet children({ props })}
+						<Form.Label>Name</Form.Label>
+						<Input placeholder="Your Name" {...props} bind:value={$formData.name} />
+					{/snippet}
+				</Form.Control>
+			</Form.Field>
+			<Form.Field {form} name="email">
+				<Form.Control>
+					{#snippet children({ props })}
+						<Form.Label>Email</Form.Label>
+						<Input placeholder="Your Email" {...props} bind:value={$formData.email} />
+					{/snippet}
+				</Form.Control>
+			</Form.Field>
+			<Form.Field {form} name="subject">
+				<Form.Control>
+					{#snippet children({ props })}
+						<Form.Label>Subject</Form.Label>
+						<Input {...props} bind:value={$formData.subject} />
+					{/snippet}
+				</Form.Control>
+			</Form.Field>
+			<Form.Field {form} name="message">
+				<Form.Control>
+					{#snippet children({ props })}
+						<Form.Label>Message</Form.Label>
+						<Textarea {...props} bind:value={$formData.message} />
+					{/snippet}
+				</Form.Control>
+			</Form.Field>
+			<Form.Field {form} name="message">
+				<Form.Button class="transition-transform duration-300 active:scale-95"
+					>Send Message</Form.Button
+				>
+			</Form.Field>
+		</form>
+	</Card.Content>
+</Card.Root>
